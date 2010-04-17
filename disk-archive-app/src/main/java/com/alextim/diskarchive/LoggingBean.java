46591536775c534ed76d7087d28af566df00282c
Aleksey Timofeev <alextim@list.ru>
2010-04-17 19:53:41 +0300

    project changes

---
+++ disk-archive-app/src/main/java/com/alextim/diskarchive/LoggingBean.java
@@ -0,0 +1,5 @@
+package com.alextim.diskarchive;
+
+public class LoggingBean {
+
+}
---
+++ disk-archive-app/src/main/java/com/alextim/diskarchive/MainFormBean.java
@@ -0,0 +1,6 @@
+package com.alextim.diskarchive;
+
+public class MainFormBean {
+	public String fullName;
+	public String shortName;
+}

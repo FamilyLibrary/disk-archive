59e5943d1d6af1e9bd19a89905756b832c93c617
Aleksey Timofeev <alextim@list.ru>
2010-04-18 16:24:45 +0300

    added version property and created getters for all properties. Also added the def values for properties

--- disk-archive-app/src/main/java/com/alextim/diskarchive/MainFormBean.java
+++ disk-archive-app/src/main/java/com/alextim/diskarchive/MainFormBean.java
@@ -1,6 +1,29 @@
 package com.alextim.diskarchive;
 
 public class MainFormBean {
-	public String fullName;
-	public String shortName;
+	public String fullName = "Aleksey Timofeev";
+	public String shortName = "A.T";
+	public String version = "1.2.1";
+	
+	
+	public String getFullName() {
+		return fullName;
+	}
+	public void setFullName(String fullName) {
+		this.fullName = fullName;
+	}
+
+	public String getShortName() {
+		return shortName;
+	}
+	public void setShortName(String shortName) {
+		this.shortName = shortName;
+	}
+
+	public String getVersion() {
+		return version;
+	}
+	public void setVersion(String version) {
+		this.version = version;
+	}
 }

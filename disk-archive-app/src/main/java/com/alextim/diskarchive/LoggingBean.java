a1114c718b601fea160432ea538c5055bbf8ba8b
Aleksey Timofeev <alextim@list.ru>
2010-04-18 16:42:05 +0300

    Added getters to the LoginBean properties

--- disk-archive-app/src/main/java/com/alextim/diskarchive/LoggingBean.java
+++ disk-archive-app/src/main/java/com/alextim/diskarchive/LoggingBean.java
@@ -1,5 +1,14 @@
 package com.alextim.diskarchive;
 
 public class LoggingBean {
-
+	private String userName;
+	private String password;
+	
+	public String getUserName() {
+		return userName;
+	}
+	
+	public String getPassword() {
+		return password;
+	}
 }

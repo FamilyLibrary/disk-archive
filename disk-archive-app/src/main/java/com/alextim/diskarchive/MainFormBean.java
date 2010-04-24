4f0f30cc66df71682569fc11e7020b99edc16934
Aleksey Timofeev <alextim@list.ru>
2010-04-24 19:32:45 +0300

    removing fullName and shortName properties and properties for them

--- disk-archive-app/src/main/java/com/alextim/diskarchive/MainFormBean.java
+++ disk-archive-app/src/main/java/com/alextim/diskarchive/MainFormBean.java
@@ -1,25 +1,8 @@
 package com.alextim.diskarchive;
 
 public class MainFormBean {
-	public String fullName;
-
-	public String shortName;
 	public String version;
 	
-	public String getFullName() {
-		return fullName;
-	}
-	public void setFullName(String fullName) {
-		this.fullName = fullName;
-	}
-
-	public String getShortName() {
-		return shortName;
-	}
-	public void setShortName(String shortName) {
-		this.shortName = shortName;
-	}
-
 	public String getVersion() {
 		return version;
 	}

f1012ff47f9e02fe3282eac74ec81f27ba8c2333
Aleksey Timofeev <aleksey.timofeev@db.com>
2012-06-03 16:19:15 +0300

    Commit uncommited changes

--- disk-archive-data/src/main/java/com/alextim/diskarchive/dao/IAuthorDAO.java
+++ disk-archive-data/src/main/java/com/alextim/diskarchive/dao/IAuthorDAO.java
@@ -2,6 +2,12 @@ package com.alextim.diskarchive.dao;
 
 import com.alextim.diskarchive.entity.Author;
 
+/**
+ * 
+ * @author Home
+ * Author DAO interface
+ * 
+ */
 public interface IAuthorDAO extends IBasicDAO<Author>{
 
 }

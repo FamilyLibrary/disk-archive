a156fd10d7ed7252efe2b38b46671965b7773b3f
Aleksey Timofeev <Aleksey_Timofeev@epam.com>
2015-09-10 19:51:12 +0300

    Fix project configuration

---
+++ disk-archive-data/src/main/java/com/alextim/diskarchive/configuration/ApplicationConfiguration.java
@@ -0,0 +1,55 @@
+package com.alextim.diskarchive.configuration;
+
+import java.io.IOException;
+import java.util.Properties;
+
+import javax.sql.DataSource;
+
+import org.hibernate.SessionFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.config.PropertiesFactoryBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.orm.hibernate3.HibernateTransactionManager;
+import org.springframework.orm.hibernate3.annotation.AnnotationSessionFactoryBean;
+import org.springframework.transaction.PlatformTransactionManager;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+
+import com.alextim.diskarchive.entity.Actor;
+import com.alextim.diskarchive.entity.Author;
+import com.alextim.diskarchive.entity.Film;
+import com.alextim.diskarchive.entity.FilmGroup;
+import com.alextim.diskarchive.entity.Series;
+
+@Configuration
+@EnableTransactionManagement
+public class ApplicationConfiguration {
+	private static final Class<?>[] ANNOTATED_CLASSES = 
+	        new Class[]{Film.class, FilmGroup.class, Series.class, Author.class, Actor.class};
+
+	@Autowired
+	private DataSource dataSource;
+	@Autowired
+	private PropertiesFactoryBean hibernateProperties;
+
+	@Bean(name = "sessionFactory")
+	public AnnotationSessionFactoryBean sessionFactory() throws IOException {
+		final AnnotationSessionFactoryBean sessionFactoryBean = new AnnotationSessionFactoryBean();
+
+		sessionFactoryBean.setDataSource(dataSource);
+		sessionFactoryBean.setAnnotatedClasses(ANNOTATED_CLASSES);
+
+		sessionFactoryBean.setHibernateProperties((Properties)hibernateProperties.getObject());
+
+		return sessionFactoryBean;
+	}
+	
+	@Bean(name = "transactionManager")
+	@Autowired
+	public PlatformTransactionManager transactionManager(SessionFactory sessionFactory) {
+		final HibernateTransactionManager tm = new HibernateTransactionManager();
+		tm.setDataSource(dataSource);
+		tm.setSessionFactory(sessionFactory);
+		return tm;
+	}
+}
--- disk-archive-webapp/src/test/java/com/alextim/diskarchive/dwr/services/impl/FilmRemoteServiceTest.java
+++ disk-archive-webapp/src/test/java/com/alextim/diskarchive/dwr/services/impl/FilmRemoteServiceTest.java
@@ -16,38 +16,27 @@ import com.alextim.diskarchive.entity.Film;
 import com.alextim.diskarchive.services.IFilmService;
 
 @RunWith(SpringJUnit4ClassRunner.class)
-@ContextConfiguration(locations={
-		"classpath:hibernate.xml",
-		"file:src/main/webapp/WEB-INF/web-application-config.xml"}, 
-		inheritLocations = true)
-public class FilmRemoteServiceTest{
-	public static final Logger log = Logger.getLogger(FilmRemoteServiceTest.class);
-	
-	@Autowired
-	IFilmService filmService;
-	
-	@Test
-	public void testAddFilm() {
-		/*Mockery context = new Mockery();
-		final IFilmRemoteService service = context.mock(IFilmRemoteService.class);
+@ContextConfiguration(locations = { "classpath:hibernate.xml",
+        "file:src/main/webapp/WEB-INF/web-application-config.xml" }, inheritLocations = true)
+public class FilmRemoteServiceTest {
+    public static final Logger log = Logger.getLogger(FilmRemoteServiceTest.class);
 
-		context.checking(new Expectations() {{
-			oneOf(service).addFilm();
-		}});*/
+    @Autowired
+    IFilmService filmService;
 
-		List<Film> films = new ArrayList<Film>();
-		
-		FilmRemoteService filmRemoteService = new FilmRemoteService();
-		filmRemoteService.setFilmService(filmService);
-		
-		films = filmService.getFilms();
-		Assert.assertEquals(0, films.size());
-		
-		filmRemoteService.addFilm();
-		
-		films = filmRemoteService.getFilmService().getFilms();
-		Assert.assertEquals(1, films.size());
-		
-		//context.assertIsSatisfied();
-	}
+    @Test
+    public void testAddFilm() {
+        List<Film> films = new ArrayList<Film>();
+
+        FilmRemoteService filmRemoteService = new FilmRemoteService();
+        filmRemoteService.setFilmService(filmService);
+
+        films = filmService.getFilms();
+        Assert.assertEquals(0, films.size());
+
+        filmRemoteService.addFilm();
+
+        films = filmRemoteService.getFilmService().getFilms();
+        Assert.assertEquals(1, films.size());
+    }
 }

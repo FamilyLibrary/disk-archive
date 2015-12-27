104d6d20fe2886f2e2d1c815fd1301f3c6146fbe
Aleksey Timofeev <alextim@list.ru>
2015-12-27 14:52:37 +0200

    Registry form fixes

--- disk-archive-webapp/src/main/webapp/js/register.js
+++ disk-archive-webapp/src/main/webapp/js/register.js
@@ -1,31 +1,18 @@
-	Ext.Loader.setConfig({
-		  enabled: true,
-		  paths:{MyApp:'js'}
-	});
-	Ext.require([
-	  'Ext.form.*',
-	  'Ext.toolbar.*',
-	  'Ext.button.*',
-	  'MyApp.view.Register'
-	]);
+Ext.Loader.setConfig({
+    enabled : true,
+    paths : {
+        MyApp : 'js'
+    }
+});
+Ext.require([ 'Ext.form.*', 'Ext.toolbar.*', 'Ext.button.*',
+        'MyApp.view.Register' ]);
 
-Ext.onReady(function(){
-  var mypanel = Ext.create('MyApp.view.Register',{
-    title:'Register',
-    renderTo: Ext.getBody(),
-items: [{
-xtype: 'numberfield',
-anchor: '60%',
-  fieldLabel: 'Customer ID'
-},{
-xtype: 'textfield',
-anchor: '-18',
-fieldLabel: 'Name'
-},{
-xtype: 'textfield',
-fieldLabel: 'Phone'
-}]
-  });
+Ext.onReady(function() {
+    var mypanel = Ext.create('MyApp.view.Register', {
+        title : 'Register',
+        renderTo : Ext.getBody(),
+        cancelUrl: "/login.html"
+    });
 
-  console.log ('Ok');
+    console.log('Ok');
 });

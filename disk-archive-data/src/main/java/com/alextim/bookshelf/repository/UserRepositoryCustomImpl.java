8bf3729af94b06b1968204645511507782be942c
sergtm1 <sergey.timofeev.2000@list.ru>
2016-07-27 20:36:58 +0300

    Fix of UserRepositoryCustomImpl according to comments (#4)
    * Fix of UserRepositoryCustomImpl according to comments
    
    * [BOOK-7]: Add logic that check old password parameter on null value in  UserRepositoryCustomImpl->changePassword

--- disk-archive-data/src/main/java/com/alextim/bookshelf/repository/UserRepositoryCustomImpl.java
+++ disk-archive-data/src/main/java/com/alextim/bookshelf/repository/UserRepositoryCustomImpl.java
@@ -1,12 +1,14 @@
 package com.alextim.bookshelf.repository;
 
-import com.alextim.bookshelf.repository.UserRepositoryCustom;
 import com.alextim.entity.User;
+import org.apache.commons.lang.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
-
+import java.lang.*;
 /**
  * Created by admin on 26.07.2016.
  */
+
+
 public class UserRepositoryCustomImpl implements UserRepositoryCustom {
 
     @Autowired
@@ -21,12 +23,16 @@ public class UserRepositoryCustomImpl implements UserRepositoryCustom {
 
     @Override
     public void changePassword(String login, String password, String newPassword) {
+        if (StringUtils.isEmpty(password)) {
+            throw new IllegalArgumentException("Old password shouldn't be empty or null");
+        }
+
         User user = userRepository.findByLogin(login);
-        if(password.equals(newPassword)) {
+        if(password.equals(user.getPassword())) {
             user.setPassword(newPassword);
             userRepository.saveAndFlush(user);
         }else{
-                throw new IllegalArgumentException();
+            throw new IllegalStateException ("New and old passwords are different");
         }
     }
 

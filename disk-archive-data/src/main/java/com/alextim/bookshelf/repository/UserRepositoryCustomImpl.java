3585c28cdf3ece836b0887c74f04678b60660d88
Sergey Timofeev <sergey.timofeev.2000@list.ru>
2016-07-27 14:50:30 +0300

    Fix of UserRepositoryCustomImpl according to comments

--- disk-archive-data/src/main/java/com/alextim/bookshelf/repository/UserRepositoryCustomImpl.java
+++ disk-archive-data/src/main/java/com/alextim/bookshelf/repository/UserRepositoryCustomImpl.java
@@ -2,11 +2,14 @@ package com.alextim.bookshelf.repository;
 
 import com.alextim.bookshelf.repository.UserRepositoryCustom;
 import com.alextim.entity.User;
+import org.apache.poi.util.StringUtil;
 import org.springframework.beans.factory.annotation.Autowired;
-
+import java.lang.*;
 /**
  * Created by admin on 26.07.2016.
  */
+
+
 public class UserRepositoryCustomImpl implements UserRepositoryCustom {
 
     @Autowired
@@ -22,11 +25,14 @@ public class UserRepositoryCustomImpl implements UserRepositoryCustom {
     @Override
     public void changePassword(String login, String password, String newPassword) {
         User user = userRepository.findByLogin(login);
-        if(password.equals(newPassword)) {
+        if(password.equals(user.getPassword())) {
             user.setPassword(newPassword);
             userRepository.saveAndFlush(user);
+        }else if (password.isEmpty()){
+            throw new IllegalArgumentException("Old password shouldn't be empty or null");
+
         }else{
-                throw new IllegalArgumentException();
+            throw new IllegalStateException ("New and old passwords are different");
         }
     }
 

617de0be091a8a92988047ccd56915b3098bb9a9
Sergey Timofeev <sergey.timofeev.2000@list.ru>
2016-08-03 20:31:50 +0300

    [BOOK-7]: implementation of UserController

--- disk-archive-webapp/src/main/java/com/alextim/diskarchive/controllers/UserController.java
+++ disk-archive-webapp/src/main/java/com/alextim/diskarchive/controllers/UserController.java
@@ -1,11 +1,36 @@
 package com.alextim.diskarchive.controllers;
 
+import com.alextim.bookshelf.service.IUserService;
+import com.alextim.entity.User;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+
 
 /**
  * Created by admin on 01.08.2016.
  */
 @Controller
+@RequestMapping("/user")
 public class UserController {
+    @Autowired
+    private IUserService userService;
 
+    @RequestMapping(value="/create", method = RequestMethod.GET)
+    @ResponseBody
+    public User createUser(){
+        User user = new User();
+        return user;
+    }
+
+    @RequestMapping(value="/save", method = RequestMethod.POST)
+    public void saveUser(User user) {
+        userService.addUser(user);
+    }
+    @RequestMapping(value = "/delete", method = RequestMethod.GET)
+    public void deleteUser(Long id){
+        userService.delete(id);
+    }
 }
